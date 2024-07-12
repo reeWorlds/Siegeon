50080b0f9205799888b9613afde105002b0ae3c2
unknown <max.koval4uk@ukr.net>
2024-07-13 01:05:08 +0300

    add game loop files

---
+++ Code/Siegeon/Siegeon/src/Abstractions/GameLoop/GameModule.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+
+
+
+
+
+
+
+// 3 threads for main loops, rendering, and physics
+// how to support double buffering?
+// think about all the processes
+// work on constants and enums and includes
+// think about namespaces
+// think about the game loop in general
+
+
+
+
+
+
